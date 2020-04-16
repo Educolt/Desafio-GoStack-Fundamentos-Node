747ac74143ca0e4fa1686e226712b51c7c2137ff
Educolt <Edupiece@hotmail.com>
2020-04-16 02:39:29 -0300

    [SOLVED]

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    // get sum of outcome transactions
+    const outcome = this.transactions.reduce((tot, item) => {
+      if (item.type === 'outcome') {
+        return tot + item.value;
+      }
+      return tot;
+    }, 0);
+
+    // get sum of income transactions
+    const income = this.transactions.reduce((tot, item) => {
+      if (item.type === 'income') {
+        return tot + item.value;
+      }
+      return tot;
+    }, 0);
+
+    // set total value equal sub income with outcome
+    const total = income - outcome;
+
+    // return balance object
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -8,8 +8,23 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    // get total balance
+    const { total } = this.transactionsRepository.getBalance();
+
+    // check if have enough balance
+    if (type === 'outcome' && value > total) {
+      throw Error('Inválid Transaction');
+    }
+
+    // create transaction object
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
